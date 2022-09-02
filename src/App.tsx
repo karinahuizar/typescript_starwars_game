4d7a0cf1d31bb35fb56797e9f506121798ad6fae
Karina Huizar <karinahuizar@gmail.com>
2022-09-02 15:27:49 -0500

    Created some states for the application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import Card from './components/Card/Card';
+// Setup
+import { createBoard } from './setup';
+import { shuffleArray } from './utils';
+// Types
+import { CardType } from './setup';
 // Styles
 import { Grid } from './App.styles';
 
 const App = () => {
+  const [cards, setCards] = React.useState<CardType[]>(shuffleArray(createBoard()));
+  const [gameWon, setGameWon] = React.useState(false);
+  const [matchedPairs, setMatchedPairs] = React.useState(0);
+  const [clickedCard, setClickedCard] = React.useState<undefined | CardType>(undefined);
+
   return (
     <div>
-      <Grid>Start Here!</Grid>
+      <Grid>
+        {cards.map(card => (
+          <Card key={card.id} card={card} callback={handleCardClick} />
+        ))}
+      </Grid>
     </div>
   );
 };
