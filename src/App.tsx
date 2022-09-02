14d44fc4e87fe5101930fe79742e50d9b1daaec2
Karina Huizar <karinahuizar@gmail.com>
2022-09-02 15:34:46 -0500

    Added functionality to the cards for the memory game.

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,49 @@ const App = () => {
   const [matchedPairs, setMatchedPairs] = React.useState(0);
   const [clickedCard, setClickedCard] = React.useState<undefined | CardType>(undefined);
 
+  React.useEffect(() => {
+    if (matchedPairs === cards.length / 2) {
+      console.log('Game Won!');
+      setGameWon(true);
+    }
+  }, [matchedPairs]);
+
   const handleCardClick = (currentClickedCard: CardType) => {
     // Flip the card
     setCards(prev =>
       prev.map(card => (card.id === currentClickedCard.id ? { ...card, flipped: true, clickable: false } : card))
     );
+    // If this is the first card that is flipped
+    // just keep it flipped
+    if (!clickedCard) {
+      setClickedCard({ ...currentClickedCard });
+      return;
+    }
+
+    // If it's a match
+    if (clickedCard.matchingCardId === currentClickedCard.id) {
+      setMatchedPairs(prev => prev + 1);
+      setCards(prev =>
+        prev.map(card =>
+          card.id === clickedCard.id || card.id === currentClickedCard.id ? { ...card, clickable: false } : card
+        )
+      );
+      setClickedCard(undefined);
+      return;
+    }
+
+    // If it's not a matched pair, wait one second and flip them back
+    setTimeout(() => {
+      setCards(prev =>
+        prev.map(card =>
+          card.id === clickedCard.id || card.id === currentClickedCard.id
+            ? { ...card, flipped: false, clickable: true }
+            : card
+        )
+      );
+    }, 1000);
+
+    setClickedCard(undefined);
   };
 
   return (
